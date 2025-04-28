d7cd12d3f33fb06ff6294b8bd9591a9d46810275
urgupdolmus <urgupdolmus@gmail.com>
2025-04-28 21:43:42 +0300

    Add files via upload

---
+++ urgupdolmussurucu/service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event) {
+  console.log('Service Worker Kuruluyor...');
+  event.waitUntil(
+    caches.open('urgup-dolmussurucu-cache').then(function(cache) {
+      return cache.addAll([
+        './urgupdolmussurucu.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
